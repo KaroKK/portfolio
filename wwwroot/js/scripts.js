f7c4a9f290660ed52e8fe68d19993ab324c36740
KaroLocal <kuster.karolina@yahoo.com>
2025-02-14 17:15:52 +0100

    css

---
+++ wwwroot/js/scripts.js
@@ -0,0 +1,15 @@
+﻿// AOS Initialization
+AOS.init({
+    duration: 900,
+    once: false,
+});
+
+// Scroll-based header animation
+document.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
