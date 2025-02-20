61ed5479d66ac1c31e53ed48578af00b7c9b90d5
KaroLocal <kuster.karolina@yahoo.com>
2025-02-20 23:35:06 +0100

    mail

--- wwwroot/js/scripts.js
+++ wwwroot/js/scripts.js
@@ -7,12 +7,14 @@ AOS.init({
 // Scroll-based header animation
 document.addEventListener("scroll", function () {
     const header = document.querySelector("header");
-    if (window.scrollY > 50) {
+    if (window.scrollY > 60) {
         header.classList.add("scrolled");
     } else {
         header.classList.remove("scrolled");
     }
 });
+
+// Cards
 
 document.addEventListener("DOMContentLoaded", () => {
     gsap.registerPlugin(ScrollTrigger);
